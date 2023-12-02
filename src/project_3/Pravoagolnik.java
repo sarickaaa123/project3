5628b36dbfd9316b96d07484790f3d217a580ac5
Sara Usevska <sarausevska42@yahoo.com>
2023-12-02 22:53:26 +0100

    project3

---
+++ src/project_3/Pravoagolnik.java
@@ -0,0 +1,25 @@
+package project_3;
+
+ import java.util.Scanner;
+
+ public class Pravoagolnik {
+ 
+	public static void main(String[]args) {
+		double str1, str2, povrsina, perimetar;
+		Scanner tastatura=new Scanner(System.in);
+		
+		System.out.println("Programata presmetuva povrsina i perimetar na pravoagolnik:");
+		
+		System.out.println("Vnesete dolzina na edna strana:");
+		str1=tastatura.nextDouble();
+		System.out.println("Vnesete dolzina na drugata strana:");
+		str2=tastatura.nextDouble();
+		
+		povrsina=str1 * str2;
+		perimetar=2*str1+2*str2;
+		
+		System.out.println();
+		System.out.println("Perimetarot e: + perimetar:");
+		System.out.println("Povrsinata e: + povrsina");
+	}
+}
